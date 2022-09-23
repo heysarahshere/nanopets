78576b536e1cdc2a7de6d85731619e93d7cd526f
heysarahshere <heysarahshere@gmail.com>
2022-09-22 17:59:26 -0700

    breed btn lg

---
+++ resources/views/partials/profile-nav.blade.php
@@ -0,0 +1,22 @@
+<div class="container">
+    <div class="row store-nav-row">
+        <a href="#">
+            <h1 class="m-4 pr-5">{{$owner->username}}'S CREATURES</h1>
+        </a>
+        <a href="#">
+            <button class="btn btn-lg store-nav-btn m-4">all</button>
+        </a>
+        <a href="#">
+            <button class="btn btn-lg store-nav-btn m-4">adults</button>
+        </a>
+        <a href="#">
+            <button class="btn btn-lg store-nav-btn m-4">babies</button>
+        </a>
+        <a href="#">
+            <button class="btn btn-lg store-nav-btn m-4">eggs</button>
+        </a>
+
+    </div>
+</div>
+
+<div class="store-banner mt-2"><h2>ALL</h2></div>
--- resources/views/user/monsters.blade.php
+++ resources/views/user/monsters.blade.php
@@ -3,11 +3,9 @@
         {{$owner->username}}'s Creatures
 @endsection
 @section('content')
+    @include('partials.profile-nav')
 
-    <div class="home p-4">
-        <h1 class="m-0">{{$owner->username}}'s Creatures</h1>
-    </div>
-    <div class="container-monsters" style="padding-bottom: 20%; align-content: space-evenly;">
+    <div class="container-monsters pt-5" style="padding-bottom: 20%; align-content: space-evenly;">
         <div class="row">
             @foreach($pets as $pet)
                 <div class="col-lg-6 col-sm-10 monster-card-front">
@@ -16,7 +14,7 @@
                 </div>
             @endforeach
         </div>
-        <button class="btn btn-danger w-100 mt-4 breed-btn">BREED ></button>
+        <button class="btn btn-danger w-100 mt-4 large-breed-btn">BREED ></button>
     </div>
 
 
