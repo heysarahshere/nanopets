83d4da8b906b24aa55e17d930fb1979999e328ee
heysarahshere <heysarahshere@gmail.com>
2022-10-06 06:11:39 -0700

    loading bars progress

--- resources/views/creatures/breed.blade.php
+++ resources/views/creatures/breed.blade.php
@@ -6,52 +6,77 @@
     @include('partials.banner-message')
     @include('partials.profile-nav')
 
-    <div class="container-monsters  pt-5" style="padding-bottom: 20%; align-content: space-evenly;">
-        <div class="row monster-card breed-main">
-            <div class="col-6 mt-5" style="height: 100%">
-                <div class="col-12 blue-breed-gradient">
-                    <div class="blue-progress-breed blue-breed-bar">
-                        <span style="width: 2%" id="blue-span"></span>
+    <div class="container-monsters " style="padding-bottom: 20%; align-content: space-evenly;">
+        <div class="monster-card breed-main pb-5">
+
+            <div class="row">
+                <div class="col-6 mt-3" style="height: 100%">
+                    <div class="col-12 blue-breed-gradient">
+                        <div class="blue-progress-breed blue-breed-bar">
+                            <span style="width: 25%" id="blue-span"></span>
+                        </div>
                     </div>
-                </div>
-                <div class="col-6 mr-auto m-5">
-                    <h2 style="color: #0060ce; text-align: center">{{ $primary->name }}</h2>
-                    <div class="store-img-container breed-blue mb-3">
-                        <div class="breed-parent col-12">
-                            <img class="breed-child"
-                                 src="{{ Storage::disk('s3')->url("images/creatures/" . $primary->species . "/" . $primary->dev_stage . "/" . $primary->element . ".png" )}}"
-                                 alt="{{ $primary->name }} Image">
+                    <div class="col-6 mr-auto m-5">
+                        <h2 style="color: #0060ce; text-align: center">{{ $primary->name }}</h2>
+                        <div class="store-img-container breed-blue mb-3">
+                            <div class="breed-parent col-12">
+                                <img class="breed-child"
+                                     src="{{ Storage::disk('s3')->url("images/creatures/" . $primary->species . "/" . $primary->dev_stage . "/" . $primary->element . ".png" )}}"
+                                     alt="{{ $primary->name }} Image">
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="purple-breed-gradient purple-breed-bar" style="position: absolute">
-                <div class="purple-progress-breed">
-                    <span style="width: 2%; margin-left: auto !important;" id="purple-span"></span>
-                </div>
-            </div>
-            <div class="col-6 mt-5" style="height: 100%">
-                <div class="col-12 pink-breed-gradient pink-breed-bar">
-                    <div class="pink-progress-breed">
-                        <span style="width: 2%; margin-left: auto !important;" id="pink-span"></span>
+
+
+                <div class="col-6 mt-3" style="height: 100%">
+
+                    <div class="col-12 pink-breed-gradient pink-breed-bar">
+                        <div class="pink-progress-breed">
+                            <span style="width: 25%; margin-left: auto !important;" id="pink-span"></span>
+                        </div>
                     </div>
-                </div>
-                <div class="col-6 ml-auto m-5">
-                    <h2 style="color: #ad084d; text-align: center">{{ $secondary->name }}</h2>
-                    <div class="store-img-container breed-pink mb-3">
-                        <div class="breed-parent col-12">
-                            <img class="breed-child"
-                                 src="{{ Storage::disk('s3')->url("images/creatures/" . $secondary->species . "/" . $secondary->dev_stage . "/" . $secondary->element . ".png" )}}"
-                                 alt="{{ $secondary->name }} Image">
+                    <div class="col-6 ml-auto m-5">
+                        <h2 style="color: #ad084d; text-align: center">{{ $secondary->name }}</h2>
+                        <div class="store-img-container breed-pink mb-3">
+                            <div class="breed-parent col-12">
+                                <img class="breed-child"
+                                     src="{{ Storage::disk('s3')->url("images/creatures/" . $secondary->species . "/" . $secondary->dev_stage . "/" . $secondary->element . ".png" )}}"
+                                     alt="{{ $secondary->name }} Image">
+                            </div>
                         </div>
                     </div>
                 </div>
+
+
             </div>
 
+            <div class="row">
+
+                <div class="col-6 mt-5 m-auto" style="height: 100%">
+
+                    <div class="col-12 purple-breed-gradient purple-breed-bar">
+                        <div class="purple-progress-breed">
+                            <span style="width: 55%;" id="pink-span"></span>
+                        </div>
+                    </div>
+                    <div class="col-6 m-auto m-5">
+                        <h2 style="color: #ad084d; text-align: center">{{ $secondary->name }}</h2>
+                        <div class="store-img-container breed-pink mb-3">
+                            <div class="breed-parent col-12">
+                                <img class="breed-child"
+                                     src="{{ Storage::disk('s3')->url("images/creatures/" . $secondary->species . "/" . $secondary->dev_stage . "/" . $secondary->element . ".png" )}}"
+                                     alt="{{ $secondary->name }} Image">
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
 
-
+            </div>
         </div>
+
+    </div>
 
     </div>
 
