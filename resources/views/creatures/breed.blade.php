a7737cfce2d71625bed2cce7d72b40ab9ce6334b
heysarahshere <heysarahshere@gmail.com>
2022-10-05 21:19:39 -0700

    middle span

--- resources/views/creatures/breed.blade.php
+++ resources/views/creatures/breed.blade.php
@@ -10,11 +10,12 @@
         <div class="row monster-card breed-main">
             <div class="col-6 mt-5" style="height: 100%">
                 <div class="col-12 blue-breed-gradient">
-                    <div class="blue-progress-breed">
+                    <div class="blue-progress-breed blue-breed-bar">
                         <span style="width: 2%" id="blue-span"></span>
                     </div>
                 </div>
                 <div class="col-6 mr-auto m-5">
+                    <h2 style="color: #0060ce; text-align: center">{{ $primary->name }}</h2>
                     <div class="store-img-container breed-blue mb-3">
                         <div class="breed-parent col-12">
                             <img class="breed-child"
@@ -24,13 +25,19 @@
                     </div>
                 </div>
             </div>
+            <div class="purple-breed-gradient purple-breed-bar" style="position: absolute">
+                <div class="purple-progress-breed">
+                    <span style="width: 2%; margin-left: auto !important;" id="purple-span"></span>
+                </div>
+            </div>
             <div class="col-6 mt-5" style="height: 100%">
-                <div class="col-12 pink-breed-gradient">
+                <div class="col-12 pink-breed-gradient pink-breed-bar">
                     <div class="pink-progress-breed">
                         <span style="width: 2%; margin-left: auto !important;" id="pink-span"></span>
                     </div>
                 </div>
                 <div class="col-6 ml-auto m-5">
+                    <h2 style="color: #ad084d; text-align: center">{{ $secondary->name }}</h2>
                     <div class="store-img-container breed-pink mb-3">
                         <div class="breed-parent col-12">
                             <img class="breed-child"
